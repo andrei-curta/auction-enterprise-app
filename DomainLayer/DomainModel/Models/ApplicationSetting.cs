aa7a5141f8518101658e87ceeee248bdac61f410
Andrei Curta <curta.andrei98@gmail.com>
2021-07-18 00:17:51 +0300

    added ApplicationSetting

---
+++ DomainLayer/DomainModel/Models/ApplicationSetting.cs
@@ -0,0 +1,17 @@
+﻿namespace DomainModel.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    class ApplicationSetting
+    {
+        public long Id { get; set; }
+
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Name { get; set; }
+
+        [Required]
+        public string value { get; set; }
+    }
+}
