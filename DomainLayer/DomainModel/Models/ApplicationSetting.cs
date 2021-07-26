c7da332c2dfa9f6def5d649c5418b9068d0d3c89
Andrei Curta <curta.andrei98@gmail.com>
2021-07-26 20:21:18 +0300

    comantat clasa ApplicationSetting

--- DomainLayer/DomainModel/Models/ApplicationSetting.cs
+++ DomainLayer/DomainModel/Models/ApplicationSetting.cs
@@ -3,14 +3,26 @@
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
 
+    /// <summary>
+    /// Manages settings used in the application.
+    /// </summary>
     public class ApplicationSetting
     {
+        /// <summary>
+        /// Gets or sets the primary key.
+        /// </summary>
         public long Id { get; set; }
 
+        /// <summary>
+        /// Gets or sets the name of the setting.
+        /// </summary>
         [Required]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string Name { get; set; }
 
+        /// <summary>
+        /// gets or sets the value of the setting.
+        /// </summary>
         [Required]
         public string Value { get; set; }
     }
