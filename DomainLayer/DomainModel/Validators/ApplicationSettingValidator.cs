af62e6200aa1818215051d9f12f900c255a761a2
Andrei Curta <curta.andrei98@gmail.com>
2021-07-26 21:03:09 +0300

    adaugat validator pentru ApplicationSetting

---
+++ DomainLayer/DomainModel/Validators/ApplicationSettingValidator.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace DomainModel.Validators
+{
+    using DomainModel.Models;
+    using FluentValidation;
+
+    /// <summary>
+    /// Validator for <see cref="ApplicationSetting"/>.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public class ApplicationSettingValidator : AbstractValidator<ApplicationSetting>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApplicationSettingValidator"/> class.
+        /// </summary>
+        public ApplicationSettingValidator()
+        {
+            this.RuleFor(x => x.Name).NotEmpty().Matches("^[a-zA-Z0-9_.-]*$")
+                .WithMessage("Only letters, numbers and - _ are allowed");
+        }
+    }
+}
