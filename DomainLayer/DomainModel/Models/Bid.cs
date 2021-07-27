4f25f6a6f1706b1d0636f174a8a1632a571469ce
Andrei Curta <curta.andrei98@gmail.com>
2021-07-27 22:58:54 +0300

    adaugat clasa oferta

---
+++ DomainLayer/DomainModel/Models/Bid.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="Bid.cs" company="Transilvania University of Brașov">
+// Copyright (c) Curta Andrei. All rights reserved.
+// </copyright>
+
+
+namespace DomainModel.Models
+{
+    using System;
+
+    /// <summary>
+    /// Class that represents a bid an user makes at an auction.
+    /// </summary>
+    public class Bid
+    {
+        /// <summary>
+        /// Gets or sets the Id for this Bid.
+        /// </summary>
+        public long Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Id of the user that placed this bid.
+        /// </summary>
+        public long UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the auctionId that the bid was placed in.
+        /// </summary>
+        public long AuctionId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date when the bid was added. Default is current datetime.
+        /// </summary>
+        public DateTime DateAdded { get; set; } = DateTime.Now;
+    }
+}
